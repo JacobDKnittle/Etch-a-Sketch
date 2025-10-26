f7763001e2fd8fef6732c45263ca651651c191dd
Jacob Knittle <jacobdknittle@gmail.com>
2025-10-26 13:36:11 -0700

    Added reset to different sizes button func

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container');
+const sizeButton = document.querySelector('.sizeButton');
 
 for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
@@ -8,3 +9,21 @@ for (let i = 0; i < 256; i++) {
     })
     container.appendChild(square);
 }
+
+sizeButton.addEventListener('click', () => {
+    container.textContent = '';
+    let size = 0;
+    do {
+        size = prompt('Choose your dimensions for the etch a sketch: ');
+    } while(size > 100)
+
+    for (let i = 0; i < size * size; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    square.style.flexBasis = 'calc(${size} / 100)';
+    square.addEventListener('mouseenter', () => {
+        square.style.backgroundColor = 'gray';
+    })
+    container.appendChild(square);
+}
+})
